bcf9719dcdc8aa5a6d76669463ac99d8f4c63390
Ralph Drake <rama.o.ralph@gmail.com>
2016-12-09 22:43:09 -0500

    Final commit

--- Final Project/player.cpp
+++ Final Project/player.cpp
@@ -1,3 +1,23 @@
+/*
+Author: Ralph Drake
+Class: CSI-140-05
+Assignment: Final Project
+Date Assigned: November 29th, 2016
+Due Date: 17:00 EST, December 9th, 2016
+Description:
+    A text-based exploration game
+Certification of Authenticity:
+    I certify that this is entirely my own work, except where I have given
+    fully-documented references to the work of others. I understand the
+    definition and consequences of plagiarism and acknowledge that the assessor
+    of this assignment may, for the purpose of assessing this assignment:
+    - Reproduce this assignment and provide a copy to another member of
+      academic staff; and/or
+    - Communicate a copy of this assignment to a plagiarism checking
+      service (which may then retain a copy of this assignment on its
+      database for the purpose of future plagiarism checking)
+*/
+
 #include "player.h"
 
 using movement = player::movement;
